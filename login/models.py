b17b973e0fc5ac346b79517835d84c8c2f221155
Thomas Thyparampil <thomasthyparampil@gmail.com>
2017-03-23 00:05:57 +0530

    Clientid, cipher, hash generation

--- login/models.py
+++ login/models.py
@@ -5,6 +5,9 @@ class LoginDetails(models.Model):
 	username = models.CharField(max_length=50)
 	password = models.CharField(max_length=50)
 	designation = models.IntegerField(default=1)
+	clientid = models.CharField(max_length=17, default="xyz")
+	cipher_text = models.CharField(max_length=25, default="xyz")
+	hash_text = models.CharField(max_length=65, default="xyz")
 
 	def __str__(self):
 		return self.username
--- login/views.py
+++ login/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import LoginDetailsForm
 from .models import LoginDetails
+from django.contrib.auth.decorators import login_required
+from Crypto.Cipher import AES
+import uuid #for generation of client id
+import codecs
+import hashlib
 # Create your views here.
 
 def login_form(request):
@@ -22,3 +27,30 @@ def login_form(request):
 	else:
 		form = LoginDetailsForm()
 	return render(request, 'login/index.html', {'form':form})
+
+@login_required
+def login_assign(request):
+	clientid = uuid.uuid4().hex[:16].upper()
+
+	encryption_suite = AES.new('this is a key123',AES.MODE_CBC,'this is an IV456')
+	cipher_text = encryption_suite.encrypt(clientid)
+	base64_data = codecs.encode(cipher_text,'base64')
+	asc = base64_data.decode('utf-8') #cipher text to readable string format
+
+	clientid1 = clientid.encode('utf-8')
+	print(clientid1)
+	hash_object = hashlib.sha512(clientid1)
+	hex_dig = hash_object.hexdigest() #hash
+
+	#to update the last record with the above values
+	q = LoginDetails.objects.last()
+	if q.clientid == "xyz":
+		q.clientid = clientid
+		q.cipher_text = asc
+		q.hash_text = hex_dig
+		q.save()
+	else:
+		pass
+	return HttpResponse("client id, ciphertext, and hash generated")
+
+
