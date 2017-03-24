f3105a4bf586076fbac9c48376f98c15a8a4ef54
Thomas Thyparampil <thomasthyparampil@gmail.com>
2017-03-24 11:29:17 +0530

    For deployment, pythonanywhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7df--nqa%b+sa+4t3oz%4)e0fq(m*dk!96s0@j%lyj!v1&wg8y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
