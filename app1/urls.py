ba97ccde9e25248d10f91f129dbc5cd792821675
Thomas Thyparampil <thomasthyparampil@gmail.com>
2017-03-29 23:01:38 +0530

    UI and Security Fix

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,5 @@ from . import views
 urlpatterns = [
 	url(r'^userhome', views.userhome),
 	url(r'^detectorhome', views.detectorhome),
-	url(r'^displaychangepwd', views.displaychangepwd, name='displaychangepwd'),
 	url(r'^changepassword', views.changepassword),
 ]
--- app1/views.py
+++ app1/views.py
@@ -2,39 +2,53 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect	
 from .forms import ChangepwdForm
 from login.models import LoginDetails
+from django.views.decorators.cache import cache_control
 # Create your views here.
-def userhome(request):
-	username = request.session['username']
-	designation = request.session['access']
-	clientid = request.session['clientid']
-	levels = ['public','private','confidential','topsecret']
-	context = {
-	'username' : username,
-	'designation' : levels[designation%4 - 1]
 
-	}
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def userhome(request):
+	try:
+		username = request.session['username']
+		designation = request.session['access']
+		clientid = request.session['clientid']
+		levels = ['public','private','confidential','topsecret']
+		context = {
+		'username' : username,
+		'designation' : levels[designation%4 - 1],
+		'nbar' : 'home',
+		}
+	except:
+		return HttpResponseRedirect('/')
+	if designation == 5:
+		del request.session['username']
+		return HttpResponseRedirect('/')
 	return render(request, 'app1/userHome.html', context)
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def detectorhome(request):
-	username = request.session['username']
-	designation = request.session['access']
-	clientid = request.session['clientid']
+	try:
+		username = request.session['username']
+		designation = request.session['access']
+		clientid = request.session['clientid']
+		context = {
+		'username' : username,
+		'nbar' : 'home',
+		}
+	except:
+		return HttpResponseRedirect('/')
 	if designation != 5:
 		del request.session['username'] #end the session
-		return HttpResponseRedirect('/') #redirect to login page
-	context = {
-	'username' : username,
-	}
+		return HttpResponseRedirect('/') #redirect to login page	
 	return render(request, 'app1/detectorHome.html', context)
 
-def displaychangepwd(request):
-	designation = request.session['access']
-	return HttpResponseRedirect('/user/changepassword/')
-
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def changepassword(request):
-	username = request.session['username']
-	designation = request.session['access']
-	clientid = request.session['clientid']
+	try:
+		username = request.session['username']
+		designation = request.session['access']
+		clientid = request.session['clientid']
+	except:
+		return HttpResponseRedirect('/')
 	levels = ['public','private','confidential','topsecret']	
 	if request.method == 'POST':
 		form = ChangepwdForm(request.POST)
@@ -58,9 +72,10 @@ def changepassword(request):
 	context = {
 		'form' : form,
 		'username' : username,
-		'designation' : levels[designation%4 -1]
+		'designation' : levels[designation%4 -1],
+		'nbar' : 'changepass'
 	}
-	if designation ==5:
+	if designation == 5:
 		return render(request, 'app1/detector_changePassword.html', context)
 	else:
 		return render(request, 'app1/user_changePassword.html', context)
